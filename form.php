8867455bea6465e0843b839bf18e788ce33bfe2b
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-12-04 07:47:55 +0100

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -1,22 +1,24 @@
 <!doctype html>
 <html>
     <head>
+        <meta charset="UTF8">
         <title>form.php</title>
     </head>
     <body>
-        <form action="form.php/$email" method="POST" target="_blank">
-            <input type="email" filter_var name="email" id="mail" placeholder="Voer je e-mailadres in">
+        <form method="POST" target="_blank">
+            <input type="email" filter_var name="email" id="mail" placeholder="Voer je e-mailadres in" required>
             <input type="submit">
             
         </form>
         <h1>
             <?php
-                $email = htmlspecialchars($_POST["email"]);
-                if((filter_var($email, FILTER_VALIDATE_EMAIL))) {
+            if (isset($_POST["email"])) {
+                if((filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))) {
                     echo "Geldig e-mailadres!";
                 } else {
                     echo "Voer een GELDIG e-mailadres in!";
                 }
+            }
             ?>
         </h1>
     </body>
